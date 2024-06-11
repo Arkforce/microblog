3489afd92384abf09062c831a57940dcd1d5edb4
Aziz Khodzhaev <khodzhaev@gmail.com>
2024-06-11 15:44:46 -0500

    ver1

--- app.py
+++ app.py
@@ -1,29 +1,38 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-# In-memory database for posts
-posts = []
-next_id = 1
+# Database model
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return f'<Post {self.id}>'
 
 @app.route('/')
 def index():
+    posts = Post.query.all()
     return render_template('index.html', posts=posts)
 
 @app.route('/add', methods=['POST'])
 def add_post():
-    global next_id
-    title = request.form.get('title')
     content = request.form.get('content')
-    if title and content:
-        posts.append({'id': next_id, 'title': title, 'content': content})
-        next_id += 1
+    if content:
+        new_post = Post(content=content)
+        db.session.add(new_post)
+        db.session.commit()
     return redirect(url_for('index'))
 
 @app.route('/delete/<int:post_id>', methods=['POST'])
 def delete_post(post_id):
-    global posts
-    posts = [post for post in posts if post['id'] != post_id]
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
